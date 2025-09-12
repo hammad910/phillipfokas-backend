736cb8e46a5860324234a0ea4ae4b4bbb8321a56
hammad910 <hammadfaisal0000yahoo@gmail.com>
2025-09-13 04:50:05 +0500

    cors package

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import Stripe from "stripe";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
+const cors = require('cors');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,6 +14,11 @@ const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
+app.use(cors({
+    origin: 'https://phillipfokas-frontend.vercel.app',
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    credentials: true
+}));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(express.json());
