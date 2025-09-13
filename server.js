436790c193d4e8629d9f4d7ab4a49202ae8bb892
hammad910 <hammadfaisal0000yahoo@gmail.com>
2025-09-13 05:12:59 +0500

    cors

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ import Stripe from "stripe";
 import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
-const cors = require('cors');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
